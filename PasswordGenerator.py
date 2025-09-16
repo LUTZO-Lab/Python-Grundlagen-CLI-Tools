68426eeac4fe013cd3794c5ab3a34adcd3a0d9f7
LUTZO-Lab <tyarcklutz04@gmail.com>
2025-09-16 17:06:08 +0200

    Create Password Gen.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,37 @@
 #In diesem mini-Projekt möchte ich einen password generator erstellen.
 import random as rm
 
+ListOfCharacters = []
+
+with open("PasswordLiberie.txt", "r") as file:
+    characters = file.read()
+    for i in characters:
+        ListOfCharacters.append(i)
+
+
+def randomNum():
+    global ListOfCharacters
+    return rm.randint(0, len(ListOfCharacters) - 1)
+
+def generate_password(length):
+    Password = ""
+    Uppercase = False
+    Lowercase = False
+    Number = False
+    SpecialCharacter = False
+    for i in range(length):
+        Password += ListOfCharacters[randomNum()]
+        if Password[i].isupper():
+            Uppercase = True
+        elif Password[i].islower():
+            Lowercase = True
+        elif Password[i].isdigit():
+            Number = True
+        else:
+            SpecialCharacter = True
+    if not Uppercase or not Lowercase or not Number or not SpecialCharacter:
+        return generate_password(length)
+    return Password
+
+
 
