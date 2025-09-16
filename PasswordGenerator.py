6c5ac12f094b9c0b680b6de60f014ce0c94c343d
LUTZO-Lab <tyarcklutz04@gmail.com>
2025-09-16 15:17:38 +0200

    reroll projekt. make it simple

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,16 +1,4 @@
 #In diesem mini-Projekt möchte ich einen password generator erstellen.
-# ! Wichtig: Ich möchte veruchen als Seed eine richtige zufallige Zahl zu verwenden.
-# import random wird nicht verwendet, da es kein echter Zufall ist.
-
-# Als Quelle für den Seed, werde ich die aktuelle Zeit in Nanosekunden verwenden.
-# + den hash eines generierten Objektes.
-
-# Wahrscheinlich werde ich auch MS BCryptGenRandom verwenden.
-
-def get_seed():
-    import subprocess
-    result = subprocess.run("(Get-Process -Id $PID).CPU", capture_output=True, text=True, shell=True)
-    print(result.stderr)
+import random as rm
 
 
-get_seed()
